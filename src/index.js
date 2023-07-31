93ff3d03fb7b14775880bcb0f9899b5d31ad856a
Robinee <yb6925@naver.com>
2023-07-31 19:42:37 +0900

    Finish basic for upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('hihihihi');
+import './style.css';
+import { pageOne, pageTwo, pageThree } from './pages.js';
+// main page
+
+window.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('container');
+
+  const initialPage = `<nav class="navbar">
+    <ul class="links">
+      <li class="link">One</li>
+      <li class="link">Two</li>
+      <li class="link">Three</li>
+    </ul>
+    </nav>
+    <main>
+    ${pageOne}
+    </main>`;
+  container.innerHTML = initialPage;
+  const links = document.querySelectorAll('.link');
+
+  links.forEach((link) => {
+    link.addEventListener('click', test);
+  });
+});
+
+function test(e) {
+  const target = e.currentTarget.textContent;
+  const main = document.querySelector('main');
+
+  switch (target) {
+    case 'One':
+      main.innerHTML = pageOne;
+      break;
+    case 'Two':
+      main.innerHTML = pageTwo;
+      break;
+    case 'Three':
+      main.innerHTML = pageThree;
+      break;
+  }
+}
---
+++ src/pages.js
@@ -0,0 +1,13 @@
+const pageOne = `<section class="content">
+<h1>One</h1>
+</section>`;
+
+const pageTwo = `<section class="content">
+<h1>Two</h1>
+</section>`;
+
+const pageThree = `<section class="content">
+<h1>Three</h1>
+</section>`;
+
+export { pageOne, pageTwo, pageThree };
